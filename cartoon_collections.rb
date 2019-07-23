7dc5bfddc32e3f899675a14c974d2abb230aa5c4
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-23 15:55:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(calls)
-  calls.include? {|x| x.length > 4}
+  calls.any? {|x| x.length > 4}
 end
 
 def find_the_cheese# code an argument here
